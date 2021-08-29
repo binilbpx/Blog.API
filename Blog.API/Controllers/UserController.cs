f306d26a79c68527dc0a23c210c09ad48c595bdb
binilbpx <binilbpx@gmail.com>
2021-08-29 14:25:51 +0530

    Fixed bugs due to reference

--- Blog.API/Controllers/UserController.cs
+++ Blog.API/Controllers/UserController.cs
@@ -2,10 +2,10 @@
 using Blog.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
