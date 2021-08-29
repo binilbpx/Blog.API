14d4b5e7105f8967be5b1575220ca1bbb5d8a054
binilbpx <binilbpx@gmail.com>
2021-08-29 14:21:34 +0530

    Added JWT authentication

--- Blog.API/Controllers/UserController.cs
+++ Blog.API/Controllers/UserController.cs
@@ -1,9 +1,14 @@
 ﻿using Blog.API.Models;
 using Blog.API.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.API.Controllers
@@ -12,12 +17,14 @@ namespace Blog.API.Controllers
     {
         #region Property  
         private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
         #endregion
 
         #region Constructor  
-        public UserController(IUserService userService)
+        public UserController(IUserService userService, IConfiguration configuration)
         {
             _userService = userService;
+            _configuration = configuration;
         }
         #endregion
 
@@ -27,6 +34,39 @@ namespace Blog.API.Controllers
             var result = await _userService.SaveUser(user);
 
             return result;
+        }
+
+        [HttpPost("/user/login")]
+        public async Task<IActionResult> Login([FromBody] User user)
+        {
+            var result = await _userService.ValidateUser(user);
+
+            if (result)
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(3),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+
+            return Unauthorized();
         }
     }
 }
